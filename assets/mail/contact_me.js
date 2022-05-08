69688f40991145a84a6493dd12fb44b11d51bd6e
osicraft1 <64375983+osicraft1@users.noreply.github.com>
2022-05-08 10:56:14 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://psol6migy9.execute-api.eu-west-1.amazonaws.com/Lambda_DataPortfolio}",
+                url: "https://psol6migy9.execute-api.eu-west-1.amazonaws.com/Lambda_DataPortfolio",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
